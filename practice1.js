0e3b58815ac9323f342c84abcf70c08c84298365
chaitanyasivathmika19 <sivathmikayalamandala33@gmail.com>
2025-08-21 11:23:15 +0530

    modified

--- practice1.js
+++ practice1.js
@@ -1,7 +1,18 @@
-let sample = document.getElementById("demo");
-sample.innerText= "This is my first JavaScript code"
+document.writeln("This is my first JavaScript code");
 
+let sample = document.getElementById("demo");
+sample.innerText= "This is through innerText";
+
+let sam1 = document.getElementById("demo1");
+sam1.innerHTML = "This is through innerHTML "
+
+window.alert("click ok this is through alert box");
+// alert("ok") //can use in anyway
+let a = window.prompt("Enter a message");
+
+document.writeln("<br>"+a);
+// window.print("print");
+// used to print the document
 
 console.log("Basic Print method in JavaScript");
 console.log("Helooooo.....")
-alert("ok")
