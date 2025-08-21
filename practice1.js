98bbbef336f03927c1d4cab32428dfdbbb553e63
chaitanyasivathmika19 <sivathmikayalamandala33@gmail.com>
2025-08-21 11:12:31 +0530

    files added

---
+++ practice1.js
@@ -0,0 +1,7 @@
+let sample = document.getElementById("demo");
+sample.innerText= "This is my first JavaScript code"
+
+
+console.log("Basic Print method in JavaScript");
+console.log("Helooooo.....")
+alert("ok")
